031af746aab88457a4230185299c208f2bb96630
unknown <rfreddystiven23@gmail.com>
2022-07-06 22:41:47 -0500

    completo login

--- login-react/src/components/Login.jsx
+++ login-react/src/components/Login.jsx
@@ -1,28 +1,55 @@
-import React from 'react'
-import '../logincss.css';
-import './jslogin.js'
+import React, { useState } from 'react'
+import Menu from './Menu';
 
-const Login = () => {
+import '../logincss.css';
+import '../jslogin'
+
+export const Login = () => {
+  const [miLogin, setMilogin] = useState("false");
+  const [cor, setcor] = useState("");
+  const [pas, setpas] = useState("");
+
+  function iniciarSesion(e) {
+    e.preventDefault();
+    var txtcor = document.getElementById("txtcor").value;
+    var txtpas = document.getElementById("txtpas").value;
+    if (txtcor.length === 0 || txtpas.length === 0) {
+      alert("Completar los campos faltantes");
+    } else {
+      if (cor === "admin@gmail.com" && pas === "12345") {
+        setMilogin("true");
+        document.getElementById("form-login").style.display = "none";
+      } else {
+        setMilogin("false");
+        alert("Error de correo o contraseña");
+        document.getElementById("txtcor").value = "";
+        document.getElementById("txtpas").value = "";
+        document.getElementById("txtcor").focus();
+      }
+    }
+  }
+
   return (
 
     <div className="container">
 
 
+      
+      <form id='form-login'>
       <h1>LOGIN</h1>
-      <form >
         <div className="form-control">
-          <input type="text" required />
+          <input type="text" id='txtcor' onChange={(e) => setcor(e.target.value)} required />
           <label for="">Correo electronico</label>
         </div>
         <div className="form-control">
-          <input type="password" required />
+          <input type="password" id='txtpas' onChange={(e) => setpas(e.target.value)} required />
           <label for="">Contraseña</label>
         </div><br />
 
-        <input type="submit" className="btn" value="Login" />
+        <input type="submit" className="btn" value="Login" onClick={ iniciarSesion} />
 
       </form>
-
+    { miLogin === "true" && <Menu cor={cor} />}
     </div>
 
   )
--- login-react/src/components/Menu.jsx
+++ login-react/src/components/Menu.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 
-const Menu = () => {
+const Menu = (props) => {
+
+  function cerrarSesion() {
+
+    document.getElementById('caja_menu').style.display = 'none';
+    document.getElementById('form-login').style.display = 'block';
+    document.getElementById("txtcor").value = '';
+    document.getElementById("txtpas").value = '';
+    document.getElementById("txtcor").focus();
+  }
+
   return (
-    <div>Menu</div>
+
+    <>
+
+      <div id="caja_menu" style={{ textAlign: "center" }}>
+
+        <strong className="h3">
+          Bienvenido Usuario con Correo: {props.cor}
+        </strong>
+
+        <nav style={{ marginTop: 20 }}>
+          <div>
+            <a style={{ color: "blue" }} href=" " onClick={cerrarSesion}  >Cerrar Sesión</a>
+          </div>
+        </nav>
+      </div>
+
+    </>
+
   )
 }
 
